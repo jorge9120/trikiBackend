8ed9797db4fef821995aaa99993279353e2d999b
Jorge Andres Sanchez Carrillo <jasanchez@personalsoft.com.co>
2020-03-07 22:01:24 -0500

    cambio matriz

--- src/main/java/com/example/demoSemillero/controller/testController.java
+++ src/main/java/com/example/demoSemillero/controller/testController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test")
 public class testController {
 	int i=0;
-	int m[][]= new int[3][3];
+	String m[][]= new String[3][3];
 	
 	@GetMapping(value="holaMundo", produces=MediaType.APPLICATION_JSON_VALUE)
 	public String holaMundo() {
@@ -31,117 +31,116 @@ public class testController {
 	}
 	
 	@GetMapping(value="triqui", produces=MediaType.APPLICATION_JSON_VALUE)
-	public int[][] triqui() {
-		this.m[0][0] = 0;
-		this.m[0][1] = 0;
-		this.m[0][2] = 0;
-		this.m[1][0] = 0;
-		this.m[1][1] = 0;
-		this.m[1][2] = 0;
-		this.m[2][0] = 0;
-		this.m[2][1] = 0;
-		this.m[2][2] = 0;
+	public String[][] triqui() {
+		this.m[0][0] = "_";
+		this.m[0][1] = "_";
+		this.m[0][2] = "_";
+		this.m[1][0] = "_";
+		this.m[1][1] = "_";
+		this.m[1][2] = "_";
+		this.m[2][0] = "_";
+		this.m[2][1] = "_";
+		this.m[2][2] = "_";
 		return m;
 	}
 	
 	@GetMapping(value="llenarTriqui", produces=MediaType.APPLICATION_JSON_VALUE)
-	public int[][] llenarTriqui(@PathParam("x") String x,@PathParam("y") String y,@PathParam("t") String t) {
+	public String[][] llenarTriqui(@PathParam("x") String x,@PathParam("y") String y,@PathParam("t") String t) {
 		/*String t=turno.toString();
 		String x=xval.toString();
 		String y=yval.toString();*/
 		System.out.println(x+" "+y+" "+t);
-		if(m[Integer.parseInt(x)][Integer.parseInt(y)]==0) 
-		m[Integer.parseInt(x)][Integer.parseInt(y)]=Integer.parseInt(t);
+		if(m[Integer.parseInt(x)][Integer.parseInt(y)].equals("_")) 
+		m[Integer.parseInt(x)][Integer.parseInt(y)]=t;
 		
 		return m;
-		
 		
 	}
 	
 	@GetMapping(value="ganador", produces=MediaType.APPLICATION_JSON_VALUE)
 	public String ganador() {
 		
-		if (this.m[0][0]==1 
-				&& this.m[0][1]==1 
-				&& this.m[0][2]==1) {
+		if (this.m[0][0].equals("1") 
+				&& this.m[0][1].equals("1") 
+				&& this.m[0][2].equals("1")) {
 			return "gana 1";
 		}
-		if (this.m[1][0]==1 
-				&& this.m[1][1]==1 
-				&& this.m[1][2]==1) {
+		if (this.m[1][0].equals("1") 
+				&& this.m[1][1].equals("1") 
+				&& this.m[1][2].equals("1")) {
 			return "gana 1";
 		}
-		if (this.m[2][0]==1 
-				&& this.m[2][1]==1 
-				&& this.m[2][2]==1) {
+		if (this.m[2][0].equals("1") 
+				&& this.m[2][1].equals("1") 
+				&& this.m[2][2].equals("1")) {
 			return "gana 1";
 		}
-		if (this.m[0][0]==1 
-				&& this.m[1][0]==1 
-				&& this.m[2][0]==1) {
+		if (this.m[0][0].equals("1") 
+				&& this.m[1][0].equals("1") 
+				&& this.m[2][0].equals("1")) {
 			return "gana 1";
 		}
-		if (this.m[0][1]==1 
-				&& this.m[1][1]==1 
-				&& this.m[2][1]==1) {
+		if (this.m[0][1].equals("1") 
+				&& this.m[1][1].equals("1") 
+				&& this.m[2][1].equals("1")) {
 			return "gana 1";
 		}
-		if (this.m[0][2]==1 
-				&& this.m[1][2]==1 
-				&& this.m[2][2]==1) {
+		if (this.m[0][2].equals("1") 
+				&& this.m[1][2].equals("1") 
+				&& this.m[2][2].equals("1")) {
 			return "gana 1";
 		}
-		if (this.m[0][0]==1 
-				&& this.m[1][1]==1 
-				&& this.m[2][2]==1) {
+		if (this.m[0][0].equals("1") 
+				&& this.m[1][1].equals("1") 
+				&& this.m[2][2].equals("1")) {
 			return "gana 1";
 		}
-		if (this.m[2][0]==1 
-				&& this.m[1][1]==1 
-				&& this.m[0][2]==1) {
+		if (this.m[2][0].equals("1") 
+				&& this.m[1][1].equals("1") 
+				&& this.m[0][2].equals("1")) {
 			return "gana 1";
 		}
 		
 		
 		
-		if (this.m[0][0]==2 
-				&& this.m[0][1]==2 
-				&& this.m[0][2]==2) {
+		if (this.m[0][0].equals("2") 
+				&& this.m[0][1].equals("2") 
+				&& this.m[0][2].equals("2")) {
 			return "gana 2";
 		}
-		if (this.m[1][0]==2 
-				&& this.m[1][1]==2 
-				&& this.m[1][2]==2) {
+		if (this.m[1][0].equals("2") 
+				&& this.m[1][1].equals("2") 
+				&& this.m[1][2].equals("2")) {
 			return "gana 2";
 		}
-		if (this.m[2][0]==2
-				&& this.m[2][1]==2 
-				&& this.m[2][2]==2) {
+		if (this.m[2][0].equals("2")
+				&& this.m[2][1].equals("2") 
+				&& this.m[2][2].equals("2")) {
 			return "gana 2";
 		}
-		if (this.m[0][0]==2 
-				&& this.m[1][0]==2 
-				&& this.m[2][0]==2) {
+		if (this.m[0][0].equals("2") 
+				&& this.m[1][0].equals("2") 
+				&& this.m[2][0].equals("2")) {
 			return "gana 2";
 		}
-		if (this.m[0][1]==2 
-				&& this.m[1][1]==2 
-				&& this.m[2][1]==2) {
+		if (this.m[0][1].equals("2")
+				&& this.m[1][1].equals("2") 
+				&& this.m[2][1].equals("2")) {
 			return "gana 2";
 		}
-		if (this.m[0][2]==2 
-				&& this.m[1][2]==2 
-				&& this.m[2][2]==2) {
+		if (this.m[0][2].equals("2") 
+				&& this.m[1][2].equals("2") 
+				&& this.m[2][2].equals("2")) {
 			return "gana 2";
 		}
-		if (this.m[0][0]==2 
-				&& this.m[1][1]==2 
-				&& this.m[2][2]==2) {
+		if (this.m[0][0].equals("2") 
+				&& this.m[1][1].equals("2") 
+				&& this.m[2][2].equals("2")) {
 			return "gana 2";
 		}
-		if (this.m[2][0]==2 
-				&& this.m[1][1]==2 
-				&& this.m[0][2]==2) {
+		if (this.m[2][0].equals("2") 
+				&& this.m[1][1].equals("2") 
+				&& this.m[0][2].equals("2")) {
 			return "gana 2";
 		}
 		
@@ -154,10 +153,10 @@ public class testController {
 		
 	}
 	
-	public boolean termino(int [][]m) {
+	public boolean termino(String [][]m) {
 		for (int i = 0; i < this.m.length; i++) {
 			for (int j = 0; j < this.m[i].length; j++) {
-				if(m[i][j]==0) {
+				if(m[i][j].equals("_")) {
 					return false;
 				}
 			}
